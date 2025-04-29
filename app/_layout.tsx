e9fb2d5be4b23a9419c9245607019f0bb13e1f83
dbfladl1 <dbfla9678@naver.com>
2025-04-29 20:19:02 +0900

    chore: remove Stack header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,32 +8,26 @@ export const GlobalFontContext = React.createContext({ fontFamily: "Roboto" });
 export default function RootLayout() {
   console.log("🟢 앱 진입 성공");
   return (
-      <GlobalFontContext.Provider value={{ fontFamily: "DMSans" }}>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <Stack
-            screenOptions={{
-              headerStyle: {
-                backgroundColor: "#f4511e",
-              },
-              headerTintColor: "#fff",
-              headerTitleStyle: {
-                fontWeight: "bold",
-              },
-            }}
-          >
-            <Stack.Screen name="/" />
-            <Stack.Screen name="details" />
-            <Stack.Screen name="/user/login" />
-            <Stack.Screen name="/sns/snsWrite" />
-            <Stack.Screen name="/map/FindingPet" />
-            <Stack.Screen name="/map/FindingPetRegister" />
-            <Stack.Screen name="/map/FindingStore" />
-            <Stack.Screen name="/user/modify/ModifyInfo" />
-            <Stack.Screen name="/help/ChatAi" />
-            <Stack.Screen name="/help/AddPet" />
-            <Stack.Screen name="/sns/snsFeed" />
-          </Stack>
-        </GestureHandlerRootView>
-      </GlobalFontContext.Provider>
+    <GlobalFontContext.Provider value={{ fontFamily: "DMSans" }}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="/" />
+          <Stack.Screen name="details" />
+          <Stack.Screen name="/user/login" />
+          <Stack.Screen name="/sns/snsWrite" />
+          <Stack.Screen name="/map/FindingPet" />
+          <Stack.Screen name="/map/FindingPetRegister" />
+          <Stack.Screen name="/map/FindingStore" />
+          <Stack.Screen name="/user/modify/ModifyInfo" />
+          <Stack.Screen name="/help/ChatAi" />
+          <Stack.Screen name="/help/AddPet" />
+          <Stack.Screen name="/sns/snsFeed" />
+        </Stack>
+      </GestureHandlerRootView>
+    </GlobalFontContext.Provider>
   );
 }
